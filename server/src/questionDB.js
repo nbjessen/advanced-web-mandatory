dab8b2af42cec8cc3dbb064da68001101e09821b
Nbjessen <nbj@jessens.com>
2020-10-22 14:30:35 +0200

    questionDB added and updated routes

---
+++ server/src/questionDB.js
@@ -0,0 +1,55 @@
+module.exports = (mongoose) => {
+    const questionsSchema = new mongoose.Schema({
+      name: String
+    });
+
+    const questionModel = mongoose.model('question', questionSchema);
+
+
+    async function getQuestions() {
+        try {
+          return await questionModel.find();
+        } catch (error) {
+          console.error("getQuestion:", error.message);
+          return {};
+        }
+      }
+
+    async function getQuestion(id) {
+        try {
+          return await questionModel.findById(id);
+        } catch (error) {
+          console.error("getQuestion:", error.message);
+          return {};
+        }
+      }
+
+
+    async function createQuestion(text) {
+        let question = new questionModel({description: text, answer: [String], like: Number, dislike: Number});
+        return question.save();
+    }
+
+    //Hvis der ikke er noge spørgsmål, lav 10 nye
+    async function bootstrap(count = 10) {
+        let l = (await getQuestions()).length;
+        console.log("Question collection size:", l);
+    
+        if (l === 0) {
+          let promises = [];
+          for (let i = 0; i < count; i++) {
+            let newQuestion = new questionModel({description: `This is question number ${i}`, answer: ["Det her er et svar"], like: 1, dislike: 2});
+            promises.push(newQuestion.save());
+          }
+          return Promise.all(promises);
+        }
+    }
+
+    return {
+    getQuestions,
+    getQuestion,
+    createQuestion,
+    bootstrap
+    }
+
+}
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,16 +1,16 @@
-module.exports = (kittenDB) => {
+module.exports = (questionDB) => {
   const express = require("express");
   const router = express.Router();
 
   /**** Routes ****/
   router.get('/', async (req, res) => {
-    const kittens = await kittenDB.getKittens(); 
-    res.json(kittens);
+    const questions = await questionDB.getQuestions(); 
+    res.json(questions);
   });
 
   router.get('/:id', async (req, res) => {
-    const kitten = await kittenDB.getKitten(req.params.id);
-    res.json(kitten);
+    const question = await questionDB.getQuestion(req.params.id);
+    res.json(question);
   });
 
   router.post('/', async (req, res) => {
